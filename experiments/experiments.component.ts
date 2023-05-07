b755c4837ddb88dfad96af3508ae681d2c7af6a0
T. Leonard <129186412+Tam-Leonard@users.noreply.github.com>
2023-05-06 23:17:05 -0500

    Add files via upload

---
+++ experiments/experiments.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiments',
+  templateUrl: './experiments.component.html',
+  styleUrls: ['./experiments.component.css']
+})
+export class ExperimentsComponent implements OnInit {
+
+  experimentBeingEdited: string;
+
+  experiments: string[] = ['Mars soil sample', 'Plant growth in habitat', 'Human bone density'];
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  add(experimentName: string) {
+    for(let experiment of this.experiments) {
+      if (experiment === experimentName){
+        return;
+      }
+    }
+    this.experiments.push(experimentName);
+  }
+
+  remove(experiment: string) {
+    let index = this.experiments.indexOf(experiment);
+    this.experiments.splice(index, 1);
+  }
+
+  edit(experiment: string) {
+    this.experimentBeingEdited = experiment;
+ }
+
+ save(name: string, experiment: string) {
+  let index = this.experiments.indexOf(experiment);
+  this.experiments[index] = name;
+ }
+  
+}
