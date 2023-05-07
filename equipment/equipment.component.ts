49605a629c28b6c0c0b953bb31c26e198a3a58c7
T. Leonard <129186412+Tam-Leonard@users.noreply.github.com>
2023-05-06 23:16:09 -0500

    Add files via upload

---
+++ equipment/equipment.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment',
+  templateUrl: './equipment.component.html',
+  styleUrls: ['./equipment.component.css']
+})
+export class EquipmentComponent implements OnInit {
+  toolBeingEdited: string;
+  equipment: string[] = ['Habitat dome', 'Drones', 'Food containers', 'Oxygen tanks'];
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  add(toolName: string) {
+    for(let tool of this.equipment) {
+      if (tool === toolName){
+        return;
+      }
+    }
+    this.equipment.push(toolName);
+  }
+
+  remove(tool: string) {
+    let index = this.equipment.indexOf(tool);
+    this.equipment.splice(index, 1);
+  }
+
+  edit(tool: string) {
+    this.toolBeingEdited = tool;
+ }
+
+ save(name: string, tool: string) {
+  let index = this.equipment.indexOf(tool);
+  this.equipment[index] = name;
+ }
+
+}
